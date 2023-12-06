78f71f79f7c0d8fd926ab360f424cbc63ad5f560
CallaAltree <152934791+CallaAltree@users.noreply.github.com>
2023-12-05 16:53:47 -0800

    Add files via upload

---
+++ calla_login.js
@@ -0,0 +1,57 @@
+
+const loginForm = document.getElementById('login-form');
+const loginButton = document.getElementById('login-form-submit');
+const loginErrorMsg = document.getElementById('login-error-msg');
+
+var strikeCounter = 0;
+let changeBackdrop;
+
+loginButton.addEventListener('click', e => {
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+
+
+
+  if (username === 'callaxoxo' && password ==='1009') {
+    alert("Don't be basic, be SLAYSIC.");
+    window.location = 'calla_homepage.html';
+  } else {
+    alert('WROOOOOOOOOOOOOOONG!');
+     strikeCounter++;
+    
+
+
+      if ( strikeCounter === 2) {
+     window.location = 'Calla_YOUNEEDTOLEAVE.html';
+
+}
+
+  }
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
